0501b375d7e7180e59c63c9e5b8dac1b21d73192
namit2405 <jainnamit34@gmail.com>
2025-07-05 15:20:12 +0530

    context2

--- ecomm/src/CartContext.jsx
+++ ecomm/src/CartContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import api from "../axios";
+import api from "./axios";
 import { useNavigate } from "react-router-dom";
 
 const CartContext = createContext();
